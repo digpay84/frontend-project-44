a3187a1a2fd7b0d1feef08aeeab310178b93022e
Dmitry Levchenko <lds84pro@gmail.com>
2026-01-15 00:29:05 +0300

    fix eslint errors

--- eslint.config.js
+++ eslint.config.js
@@ -3,15 +3,15 @@ import stylistic from '@stylistic/eslint-plugin'
 export default [
   {
     plugins: {
-      '@stylistic': stylistic,
+      '@stylistic': stylistic
     },
     rules: {
-      '@stylistic/quotes': ['error', 'single',],
-      '@stylistic/eol-last': ['error', 'always',],
-      '@stylistic/semi': ['error', 'never',],
-      '@stylistic/indent': ['error', 2,],
-      '@stylistic/no-trailing-spaces': ['error', { 'skipBlankLines': false, 'ignoreComments': false, },],
-      '@stylistic/comma-dangle': ['error', 'always',],
-    },
-  },
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/no-trailing-spaces': ['error', { 'skipBlankLines': false, 'ignoreComments': false }],
+      '@stylistic/comma-dangle': ['error', 'never']
+    }
+  }
 ]
