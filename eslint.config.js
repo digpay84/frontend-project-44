8dd93701599c72c4bc9bc60c5b6309c6d249b106
Dmitry Levchenko <lds84pro@gmail.com>
2026-01-16 19:53:52 +0300

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,17 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import stylistic from '@stylistic/eslint-plugin'
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-]);
+export default [
+  {
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/no-trailing-spaces': ['error', { 'skipBlankLines': false, 'ignoreComments': false }],
+      // '@stylistic/comma-dangle': ['error', 'never']
+    }
+  }
+]
